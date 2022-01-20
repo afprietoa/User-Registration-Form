3967d15e700a992f23293e4c1cdc67c649cb685d
Andres Prieto <afprietoa@unal.edu.co>
2022-01-20 06:09:54 -0500

    Form Validations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,10 +6,10 @@ import { useForm } from '../hooks/useForm';
 import { postUser } from '../helpers/postUser';
 import { getUser } from '../helpers/getUser';
 
-let usrsObj ={}
+let usrsObj = {}
 
 export const Form = () => {
-    const [values, handleInputChange] = useForm({
+    const [values, handleInputChange, reset] = useForm({
         firstName: "",
         lastName: "",
         completeName: "",
@@ -20,37 +20,56 @@ export const Form = () => {
         nationality: "",
         createdBy: "Andrés Prieto",
     })
-    const {firstName, lastName, email, sicCode, sicCodeType, mobilePhone, nationality } = values
+    const { firstName, lastName, email, sicCode, sicCodeType, mobilePhone, nationality } = values
 
-    const {data:natls} = useAxiosNatl()
+    const { data: natls } = useAxiosNatl()
     console.log(natls)
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
+
     }
 
     const handleAdd = () => {
-        if(isFormValid()){
+        if (isFormValid()) {
 
-          values.completeName = values.firstName + ' ' + values.lastName
-           console.log(values)
-           usrsObj = getUser()
-           postUser(usrsObj)
+            values.completeName = values.firstName + ' ' + values.lastName
+            console.log(values)
+            usrsObj = getUser()
+            postUser(usrsObj)
+            reset()
         }
     }
-     const isFormValid = () =>{
-         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-         if(!regex.test(values.email)){
+    const isFormValid = () => {
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const regexNumber = /^[0-9]*$/;
+        if (values.firstName.trim().length === 0 || values.lastName.trim().length === 0 || values.email.length === 0 || values.sicCode.length === 0 || values.sicCodeType.length === 0 || values.mobilePhone.length === 0 || values.nationality.length === 0) {
             Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: '¡E-mail incorrecto!',
-          })
-          return false
-         }
+                icon: 'error',
+                title: 'Error...',
+                text: '¡Formato invalido! Proporcione un valor para el campo requerido en el formulario',
+            })
+            return false
+        } else if (!regexEmail.test(values.email)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error...',
+                text: '¡E-mail incorrecto!',
+            })
+            return false
+        } else if (!regexNumber.test(values.sicCode) || !regexNumber.test(values.mobilePhone)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error...',
+                text: '¡Número incorrecto!',
+            })
+            return false
+        }
+
+
         return true
 
-     }
+    }
 
     return (
         <div className="form-container">
@@ -65,8 +84,8 @@ export const Form = () => {
                         <div className="form-group col-md-10">
                             <label>Tipo Documento</label><br />
 
-                            <select className="form-control " name="sicCodeType" value={sicCodeType} onChange={handleInputChange} required >
-                                <option name="Seleccionar" value="none">Seleccionar</option>
+                            <select className="form-control " name="sicCodeType" value={sicCodeType} onChange={handleInputChange} >
+                                <option name="Seleccionar" value="">Seleccionar</option>
                                 <option name="cédula de ciudadanía" value="cédula de ciudadanía">cédula de ciudadanía</option>
                                 <option name="Pasaporte" value="Pasaporte">Pasaporte</option>
                                 <option name="cédula de extranjería" value="cédula de extranjería">cédula de extranjería</option>
@@ -75,26 +94,26 @@ export const Form = () => {
                         <div className="form-group col-md-10">
                             <label>Nombre(s)</label><br />
 
-                            <input className="form-control" name="firstName" value={firstName} onChange={handleInputChange} required />
+                            <input className="form-control" name="firstName" value={firstName} onChange={handleInputChange} />
                         </div>
                         <div className="form-group col-md-10">
                             <label>Nacionalidad</label><br />
 
-                            <select className="form-control " name="nationality" value={nationality} onChange={handleInputChange} required >
-                                <option name="Seleccionar" value="Seleccionar">Seleccionar</option>
+                            <select className="form-control " name="nationality" value={nationality} onChange={handleInputChange} >
+                                <option name="Seleccionar" value="">Seleccionar</option>
                                 {
-                                    natls.map((ctry, idx) =>(
+                                    natls.map((ctry, idx) => (
                                         <option key={idx} name={ctry.name} value={ctry.name}>{ctry.name}</option>
                                     ))
                                 }
-                                
+
 
                             </select>
                         </div>
                         <div className="form-group col-md-10">
                             <label>Email</label><br />
 
-                            <input className="form-control" name="email" value={email} onChange={handleInputChange} required />
+                            <input className="form-control" name="email" value={email} onChange={handleInputChange} />
                         </div>
 
 
